a4730a532a2f2d27c17544cbdf9f0579d7ba3656
michaelvannucci <michael.vannucci@wakefern.com>
2019-06-28 14:25:00 -0400

    fixed an mis-packaging of return body

--- main.js
+++ main.js
@@ -3,20 +3,13 @@ const utils = require('./util.js');
 
 async function main() {
     answer = await utils.getTask().then(res => {
-        let task = JSON.parse(res);
-        let calculation = utils.evaluate(task);
-        if(calculation) {
-
-        }
-        console.log('calculation -> ' + calculation);
-        return({"id":task.id, "response": calculation});
+        const task = JSON.parse(res);
+        const calculation = utils.evaluate(task);
+        console.log(`Task -> ${res}`);
+        return({"id":task.id, "result": calculation});
     });
-
-    console.log(typeof(answer.response));
-
     await utils.submitTask(answer).then(res => console.log(`Status -> ${res.statusCode}: ${res.body}`));
-
 }
 
-console.log(`Start evaluating, interval: ${process.env.REQUEST_INTERVAL}s...`);
+console.log(`Start evaluating, interval: ${process.env.REQUEST_INTERVAL} s...`);
 setInterval(main, process.env.REQUEST_INTERVAL*1000);
